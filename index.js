c4b011cc1db9c7a051cd93857c089ef9898ad0d4
Prasad Sabnis <prasad.sabnis@getinsured.com>
2018-04-10 12:06:22 +0530

    Initial Commit for Express

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('Express');
+var app = express();
+
+app.get('/', (req, res)=> {
+    res.send({hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
