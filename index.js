3e728f623f874411d04952a8d786a58c95d7a75c
Prasad Sabnis <prasad.sabnis@getinsured.com>
2018-04-10 16:57:43 +0530

    Sample message in Index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res)=> {
-    res.send({hi: 'there'});
+    res.send({hi: 'there', Lu: 'sasa'});
 });
 
 const PORT = process.env.PORT || 5000;
