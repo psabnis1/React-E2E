45c73cd15beebc7361f104ef6b23f0590e6ee446
Prasad Sabnis <prasad.sabnis@getinsured.com>
2018-04-12 14:02:41 +0530

    Changes for Google OAuth and related routing Code refactor is required

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const app = express();
+const keys = require('./config/keys.js');
 
-app.get('/', (req, res)=> {
-    res.send({hi: 'there', Lu: 'sasa'});
+passport.use(new GoogleStrategy({
+    clientID : keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+},
+(accessToken, refreshToken, profile, done) =>{
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
+})
+);
+
+app.get('/', (req, res) => {
+ res.send({'hi': 'There'});
 });
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope : ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
